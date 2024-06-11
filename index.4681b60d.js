47cd5453f4073353a34a459036e0c5d34512f617
Jorge Romero <jorgeromero98@outlook.com>
2024-06-11 16:57:24 -0600

    Updates

--- index.4681b60d.js
+++ index.4681b60d.js
@@ -1,9 +1,9 @@
 const e=document.getElementById("app");(async()=>{let t=await fetch("https://api.escuelajs.co/api/v1/products?offset=0&limit=10"),a=await t.json(),s=a?.map(e=>`
       <article class="Card">
         <img src="${e.images[0]}">
+        <h2>
+          ${e.title} <small>Precio $ ${e.price}</small>
+        </h2>
       </article>
-      <h2>
-        ${e.title} <small>$ ${e.price}</small>
-      </h2>
     `).join(""),i=document.createElement("section");i.classList.add("Items"),i.innerHTML=s,e.appendChild(i)})();
-//# sourceMappingURL=index.04c49678.js.map
+//# sourceMappingURL=index.4681b60d.js.map
